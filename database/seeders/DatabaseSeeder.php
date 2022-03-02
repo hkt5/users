5ca35d61fe3509f07b43cb3a8d8341d2c38d75a9
capodeputati <r3v3l@t104>
2022-03-02 15:40:43 +0100

    add statuses feature

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(RolesTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
         $this->call(StatusesTableSeeder::class);
     }
 }
