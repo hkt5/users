fd5e8222f4581f7b125ac27ad2f0cd46bb759b1c
capodeputati <r3v3l@t104>
2022-03-07 21:11:24 +0100

    add regenerate token

--- app/Services/LoginService.php
+++ app/Services/LoginService.php
@@ -123,6 +123,7 @@ class LoginService
         } elseif (!Hash::check($data['password'], $user->password)) {
             return $this->service->response(null, null, Response::HTTP_UNAUTHORIZED);
         } else {
+            $this->userRepository->updateLoginAttemps(['id' => $user->id]);
             $currentUser = $this->userRepository->updateToken([
                 'id' => $user->id,
                 'uuid' => Uuid::uuid4(),
